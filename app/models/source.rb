f681d8c3d17fa2303cd1e6d0a6c1351f72b3a202
andrewgauger <andrewgauger@L-W8026067AGY.(none)>
2012-09-19 01:31:25 -0700

    begun source model

--- app/models/source.rb
+++ app/models/source.rb
@@ -1,3 +1,29 @@
 class Source < ActiveRecord::Base
   # attr_accessible :title, :body
+
+  ##Fields
+  attr_accessible :url, :body
+
+  ##Associations
+  belongs_to :source_type
+  belongs_to :scope_type
+
+  ##Methods
+  def create_from_url(url, *args)
+  	if check_url 
+  		self.url = url
+  	end
+  end
+
+  def update_body
+	if check_url
+		#eventmachine to update body, expected 0.1.5  	
+  	end
+  end
+
+  def check_url
+  	return if (url.to_s.length ~= /http/i)
+  	raise "Object of Source created without specifying url parameter including protocol"
+  end
+
 end
--- db/migrate/20120919075945_create_sources.rb
+++ db/migrate/20120919075945_create_sources.rb
@@ -2,6 +2,14 @@ class CreateSources < ActiveRecord::Migration
   def change
     create_table :sources do |t|
 
+    	t.string :url
+    	t.text :body
+    	t.timestamp :last_checked
+    	
+    	##Relationships
+    	t.int :source_type_id
+    	t.int :scope_type_id
+
       t.timestamps
     end
   end
