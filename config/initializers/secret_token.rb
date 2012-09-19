57b6e3af9f9bbf85ea885371b460beb9a44f726c
andrewgauger <andrewgauger@L-W8026067AGY.(none)>
2012-09-19 00:51:50 -0700

    Created project outline

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ascast::Application.config.secret_token = '79b65627d2fd85b9a2313058147b544b2c45783a8e733890ec9655e16cd24417c47d1aab343e76c7e8acf3a57fd920e98204cfb934e757146e518fcd7d7ba02d'
